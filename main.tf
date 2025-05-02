21bd6d0122ceb3b0723f7eb128e6971f3e64b340
Bunny Kocharla <bunnyjoel391@gmail.com>
2025-05-02 16:25:57 +1000

    change module

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 ################################################################################
 
 module "db" {
-  source = "https://github.com/kbunnyjoel/terraform-rds-postgresql.git?ref=v1.1"
+  source = "git::https://github.com/kbunnyjoel/terraform-rds-postgresql.git?ref=v1.1"
 
   identifier = local.name
 
